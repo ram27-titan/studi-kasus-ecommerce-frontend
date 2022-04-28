07d25b9331930f45316ac76a6790f4b29149016b
Alam <wwinda180@gmail.com>
2022-04-28 13:48:37 +0700

    upload github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import WebFont from 'webfontloader';
+import Home from './components/Home/Home';
+import ProductDetails from './components/Product/ProductDetails';
+import Products from './components/Product/Products';
+import Search from './components/Product/Search';
+import LoginSignUp from './components/User/LoginSignUp';
+import store from './store';
+import { loadUser } from './actions/userAction';
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka'],
+      },
+    });
+
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/product/:id" component={ProductDetails} />
+      <Route exact path="/products" component={Products} />
+      <Route path="/products/:keyword" component={Products} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/login" component={LoginSignUp} />
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/actions/userAction.js
@@ -0,0 +1,53 @@
+import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOAD_USER_FAIL, CLEAR_ERRORS } from '../constants/userConstants';
+import axios from 'axios';
+
+// LOGIN
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const config = { headers: { 'Content-Type': 'application/json' } };
+
+    const { data } = await axios.post(`http://192.168.1.7:4000/api/v1/login`, { email, password }, config);
+
+    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
+
+// REGISTER
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
+
+    const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+
+    const { data } = await axios.post(`http://localhost:4000/api/v1/register`, userData, config);
+
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// LOAD USER
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOAD_USER_REQUEST });
+
+    const { data } = await axios.get(`http://localhost:4000/api/v1/me`);
+
+    dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
+  }
+};
+
+// Clearing Errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
